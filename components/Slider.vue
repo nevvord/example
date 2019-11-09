48dede4c9f64142408a6c651981cb99b184c7005
Nevvord <nevvord@gmail.com>
2019-11-09 21:28:48 +0300

    Change carousel to no-ssr

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="slider">
-        <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        img-width="1024"
-        img-height="480"
-        style="text-shadow: 1px 1px 2px #333;"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-        >
-        <!-- Слайды -->
-            <b-carousel-slide v-for="element in $store.state.carousel._Data" :key="element.key" class="img" :img-alt="element.name" :img-src="$store.state._ServerHttp + element.file" fluid    >
-                <div v-html="element.inner"></div>
-            </b-carousel-slide>
-        </b-carousel>
-    </div>
+    <no-ssr>
+        <div class="slider">
+            <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            img-width="1024"
+            img-height="480"
+            style="text-shadow: 1px 1px 2px #333;"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+            >
+            <!-- Слайды -->
+                <b-carousel-slide v-for="element in $store.state.carousel._Data" :key="element.key" class="img" :img-alt="element.name" :img-src="$store.state._ServerHttp + element.file" fluid    >
+                    <div v-html="element.inner"></div>
+                </b-carousel-slide>
+            </b-carousel>
+        </div>
+    </no-ssr>
 </template>
 
 <script>
